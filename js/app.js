25341f11a199f59db9302cad2c529ea5023c0f25
Jarez25 <45266911+Jarez25@users.noreply.github.com>
2021-09-10 12:38:59 -0600

    mater

---
+++ js/app.js
@@ -0,0 +1,96 @@
+// let password = "nana";
+
+// let input = "nan";
+
+// let resul = input == password;
+
+// if (resul == true){
+//     console.log("usuario correcto");
+// }
+
+// if(resul == false){
+//     console.log("usuario incorrecto")
+// // }
+
+// let score = 70;
+
+// if (score > 30) {
+//     console.log("no necesitas practica")
+// } else if (score > 15) {
+//     console.log("as mejorado")
+// } else{
+//     console.log("practica mas")
+// }
+
+// let typeCard = "debianCard"
+
+// switch (typeCard) {
+//     case "debianCard":
+//         console.log(" esta es una tarjeta de debito");
+//         break
+//     case "credit card":
+//         console.log("esta es una tarjeta de");
+//         break;
+//     default:
+// //         console.log("no tienes tarjeta")
+
+
+// let count = 50
+
+// while (count > 0){
+//     console.log(count);
+//     count = count - 1;
+// }
+
+// let count = 1;
+
+// while (count <= 50){
+//     console.log(count);
+//     count ++;
+// }
+
+// let name = ["jairo", "jose", "medina"]
+
+// for(let i = 0; i < name.length; i++ ){
+//     console.log(name[i]);
+     
+
+// }
+
+
+// let name = ["jairo", "jose", "medina"]
+
+// for(let i = name.length - 1; i >= 0; i-- ){
+//     console.log(name[i]);
+     
+    
+// }
+// let name = ["jairo", "jose", "medina"]
+
+
+// function saludo () {
+    
+// for(let i = 0; i < name.length; i++ ){
+//     console.log(name[i]);
+     
+    
+// };
+// }
+
+// saludo();
+
+function saludo(person) {
+    console.log ("hello" + person)
+}
+
+saludo(" jairo")
+saludo(" maria")
+saludo(" marcus")
+
+function add(n1, n2){
+    console.log(n1 + n2);
+}
+
+add (5, 6)
+add (10, 6)
+add (9, 9)
